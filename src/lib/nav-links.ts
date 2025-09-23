b6f0b6ad60b3be9ff75470a5f9d32500f4bf09a4
Elon Musk <sukzession1998@gmail.com>
2025-09-23 07:45:48 +0000

    https://worldsold.wixsite.com/electric-technocracy Deutsch https://worl

--- src/lib/nav-links.ts
+++ src/lib/nav-links.ts
@@ -1,12 +1,6 @@
 import type { ComponentType } from 'react';
 import {
-  Book,
   Globe,
-  LifeBuoy,
-  PlayCircle,
-  Users,
-  Dog,
-  MessageCircle,
   type LucideProps,
 } from 'lucide-react';
 
@@ -23,97 +17,27 @@ export interface NavCategory {
 
 export const navigationLinks: NavCategory[] = [
   {
-    title: 'Apps',
-    links: [
-      {
-        title: 'Pet Analyzer',
-        url: '/pet-analyzer',
-        icon: Dog,
-      },
-       {
-        title: 'Chatbot',
-        url: '/chatbot',
-        icon: MessageCircle,
-      },
-    ],
-  },
-  {
     title: 'Websites',
     links: [
       {
-        title: 'Sovereign Stack',
-        url: 'https://sovereignstack.tools/',
+        title: 'Electric Technocracy',
+        url: 'https://worldsold.wixsite.com/electric-technocracy',
         icon: Globe,
       },
       {
-        title: 'Bitcoin',
-        url: 'https://bitcoin.org/',
+        title: 'Electric Technocracy (DE)',
+        url: 'https://worldsold.wixsite.com/electric-technocracy/de',
         icon: Globe,
       },
       {
-        title: 'Nostr',
-        url: 'https://nostr.com/',
+        title: 'World Sold',
+        url: 'https://worldsold.wixsite.com/world-sold',
         icon: Globe,
       },
-    ],
-  },
-  {
-    title: 'eBooks',
-    links: [
       {
-        title: 'The Sovereign Individual',
-        url: 'https://www.amazon.com/Sovereign-Individual-Mastering-Transition-Information/dp/0684832720',
-        icon: Book,
-      },
-      {
-        title: 'The Bitcoin Standard',
-        url: 'https://www.amazon.com/Bitcoin-Standard-Decentralized-Alternative-Central/dp/1119473861',
-        icon: Book,
-      },
-    ],
-  },
-  {
-    title: 'Media',
-    links: [
-      {
-        title: 'TFTC',
-        url: 'https://tftc.io/',
-        icon: PlayCircle,
-      },
-      {
-        title: 'Citadel Dispatch',
-        url: 'https://citadeldispatch.com/',
-        icon: PlayCircle,
-      },
-    ],
-  },
-  {
-    title: 'Support',
-    links: [
-      {
-        title: 'Start9',
-        url: 'https://start9.com/',
-        icon: LifeBuoy,
-      },
-      {
-        title: 'Foundation',
-        url: 'https://foundationdevices.com/',
-        icon: LifeBuoy,
-      },
-    ],
-  },
-  {
-    title: 'UBI',
-    links: [
-      {
-        title: 'Geyser',
-        url: 'https://geyser.fund/',
-        icon: Users,
-      },
-      {
-        title: 'PlebLab',
-        url: 'https://pleblab.com/',
-        icon: Users,
+        title: 'World Sold (EN)',
+        url: 'https://worldsold.wixsite.com/world-sold/en',
+        icon: Globe,
       },
     ],
   },
