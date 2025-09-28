53254ea28a697af26d7cf9c932ca9165a494cb4d
Elon Musk <sukzession1998@gmail.com>
2025-09-28 05:03:40 +0000

    Cluster-Tags direkt im Code definieren (z. B. navigator.dnsLeaks = true)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#c026d3" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
